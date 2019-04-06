8d8ee30247bd554ae6c305c2e2425f39f1a82b9d
Rawhate <49337637+Rawhate@users.noreply.github.com>
2019-04-06 16:45:30 +0600

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -175,7 +175,7 @@ function exportMd() {
     Object.keys(template_list).map(function (index, ele) {
         var eles = template_list[index];
         mdstr += '\n#### ' + index;
-        mdstr += '\n[![](https://raw.githubusercontent.com/Furkan2514/PixelPlaceMap-v31/master/images/' + eles.name + ')]';
+        mdstr += '\n[![](https://raw.githubusercontent.com/Rawhate/PixelPlaceMap-v31/master/images/' + eles.name + ')]';
         mdstr += '(http://pixelplace.fun/@' + Math.floor(eles.x + eles.width / 2) + ',' + Math.floor(eles.y + eles.height / 2) + ')'
         mdstr += '\n';
         ttlpx += eles.width * eles.height;
